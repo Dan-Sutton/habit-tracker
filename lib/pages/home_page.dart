516c83f56f1f208a9fdde6a46cb62d241b2e4c79
Dan-Sutton <dewasutton@gmail.com>
2023-01-04 13:17:26 +0000

    checkbox changing state on tap

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:habit_tracker/components/habit_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +10,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool habitCompleted = false;
+
+  void checkBoxTapped(bool? value) {
+    setState(() {
+      habitCompleted = value!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: ListView(
+        children: [
+          // Habit Tiles
+          HabitTile(
+            habitCompleted: habitCompleted,
+            habitName: 'Morning Jog',
+            onChanged: (value) => checkBoxTapped(value),
+          )
+        ],
+      ),
+    );
   }
 }
