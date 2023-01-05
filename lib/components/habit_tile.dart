0f8bfb8ffc6e1a044e25f04129054c0a9419e87f
Dan-Sutton <dewasutton@gmail.com>
2023-01-05 14:33:20 +0000

    custom expansion list component

--- lib/components/habit_tile.dart
+++ lib/components/habit_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-class HabitTile extends StatelessWidget {
+import 'app_expansion_panel.dart';
+
+class HabitTile extends StatefulWidget {
   final String habitName;
   final bool habitCompleted;
   final Function(bool?)? onChanged;
@@ -18,57 +20,82 @@ class HabitTile extends StatelessWidget {
   });
 
   @override
+  State<HabitTile> createState() => _HabitTileState();
+}
+
+class _HabitTileState extends State<HabitTile> {
+  bool _isOpen = false;
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Slidable(
-        endActionPane: ActionPane(
-          motion: const StretchMotion(),
-          children: [
-            // settings option
-            SlidableAction(
-              onPressed: settingsTapped,
-              backgroundColor: Color.fromARGB(255, 69, 149, 69),
-              icon: Icons.settings,
-              borderRadius: BorderRadius.circular(12),
-            ),
-
-            // delete option
-            SlidableAction(
-              onPressed: deleteTapped,
-              backgroundColor: Color.fromARGB(255, 246, 93, 90),
-              icon: Icons.delete,
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ],
-        ),
-        child: Container(
-          padding: EdgeInsets.all(24),
-          decoration: BoxDecoration(
-            color: Colors.grey[100],
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          endActionPane: ActionPane(
+            motion: const StretchMotion(),
             children: [
-              // checkbox
-              Row(
-                children: [
-                  Checkbox(
-                    value: habitCompleted,
-                    onChanged: onChanged,
-                  ),
-
-                  // habit name
-                  Text(habitName),
-                ],
+              // settings option
+              SlidableAction(
+                onPressed: widget.settingsTapped,
+                backgroundColor: Color.fromARGB(255, 69, 149, 69),
+                icon: Icons.settings,
+                borderRadius: BorderRadius.circular(12),
               ),
 
-              Icon(Icons.arrow_back)
+              // delete option
+              SlidableAction(
+                onPressed: widget.deleteTapped,
+                backgroundColor: Color.fromARGB(255, 246, 93, 90),
+                icon: Icons.delete,
+                borderRadius: BorderRadius.circular(12),
+              ),
             ],
           ),
-        ),
-      ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: AppExpansionPanelList(
+              children: [
+                ExpansionPanel(
+                    backgroundColor: Colors.grey[100],
+                    canTapOnHeader: true,
+                    isExpanded: _isOpen,
+                    headerBuilder: ((context, isExpanded) {
+                      return Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // checkbox
+                            Row(
+                              children: [
+                                Checkbox(
+                                  value: widget.habitCompleted,
+                                  onChanged: widget.onChanged,
+                                ),
+
+                                // habit name
+                                Text(widget.habitName),
+                              ],
+                            ),
+
+                            Icon(Icons.arrow_back)
+                          ],
+                        ),
+                      );
+                    }),
+                    body: Row(
+                      children: [Text('YUILGWFGYUIlwGFYUIlOWEGWYUILO')],
+                    ))
+              ],
+              expansionCallback: (i, isOpen) {
+                setState(() {
+                  _isOpen = !_isOpen;
+                });
+              },
+            ),
+          )),
     );
   }
 }
